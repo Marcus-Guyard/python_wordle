70e55723efe9d1574db5152d2b1768521c92ab0f
Marcus-Guyard <marcus.g300@gmail.com>
2022-10-18 11:35:46 +0200

    test_gorest

--- testautomation/test_gorest.py
+++ testautomation/test_gorest.py
@@ -1,31 +1,15 @@
 from http import HTTPStatus
 import requests
+from pprint import pprint
 import pytest
+from gorest import api_token as api
 
 GOREST_USERS = "https://gorest.co.in/public/v2/users"
 GOREST_POSTS = "https://gorest.co.in/public/v2/posts"
 GOREST_COMMENTS = "https://gorest.co.in/public/v2/comments"
+GOREST_TODOS = "https://gorest.co.in/public/v2/todos"
 
-
-@pytest.fixture
-def api_token() -> str:
-    with open("token") as f:
-        return f.read().strip()
-
-
-@pytest.fixture
-def header():
-    return {"authorization": f"Bearer {api_token}"}
-
-HEADER = {"authorization": f"Bearer {api_token}"}
-
-
-@pytest.fixture
-def user_adata():
-    return {"name": "new tester",
-            "email": "test@email.com",
-            "gender": "male",
-            "status": "active"}
+HEADER = {"Authorization": f"Bearer {api()}"}
 
 userdata = {"name": "new tester",
             "email": "test@email.com",
@@ -38,38 +22,152 @@ def test_get_all_users():
     assert res.status_code == HTTPStatus.OK
 
 
-def test_create_user_v2():
+def test_get_all_posts():
+    res = requests.get(GOREST_POSTS, headers=HEADER)
+    assert res.status_code == HTTPStatus.OK
+
+
+def test_get_all_comments():
+    res = requests.get(GOREST_COMMENTS, headers=HEADER)
+    assert res.status_code == HTTPStatus.OK
+
+
+def test_get_all_todos():
+    res = requests.get(GOREST_POSTS, headers=HEADER)
+    assert res.status_code == HTTPStatus.OK
+
+
+def test_get_user():
+    res = requests.post(GOREST_USERS, data=userdata, headers=HEADER)
+    user_dict = res.json()
+    get_user = requests.get(GOREST_USERS + f"/{user_dict['id']}", headers=HEADER)
+    assert get_user.status_code == HTTPStatus.OK
+    requests.delete(GOREST_USERS + f"/{user_dict['id']}", headers=HEADER)
+
+
+def test_get_user_posts():
+    res = requests.post(GOREST_USERS, data=userdata, headers=HEADER)
+    user_dict = res.json()
+    requests.post(GOREST_POSTS,
+                  data={"user_id": user_dict['id'],
+                        'title': 'Min titel',
+                        'body': 'Detta är en post'},
+                  headers=HEADER)
+    get_user_posts = requests.get(GOREST_USERS + f"/{user_dict['id']}/posts", headers=HEADER)
+    assert get_user_posts.status_code == HTTPStatus.OK
+    requests.delete(GOREST_USERS + f"/{user_dict['id']}", headers=HEADER)
+
+
+def test_get_user_comments():
+    res = requests.post(GOREST_USERS, data=userdata, headers=HEADER)
+    user_dict = res.json()
+    new_post = requests.post(GOREST_POSTS,
+                             data={"user_id": user_dict['id'],
+                                   'title': 'Min titel',
+                                   'body': 'Detta är en post'},
+                             headers=HEADER)
+    post_dict = new_post.json()
+    requests.post(GOREST_COMMENTS,
+                  data={"post_id": post_dict["id"],
+                        "name": 'Commenter Name',
+                        "email": 'commenter@email.com',
+                        "body": 'This is a comment'},
+                  headers=HEADER)
+
+    get_user_comments = requests.get(GOREST_POSTS + f"/{post_dict['id']}/comments", headers=HEADER)
+    assert get_user_comments.status_code == HTTPStatus.OK
+    requests.delete(GOREST_USERS + f"/{user_dict['id']}", headers=HEADER)
+
+
+def test_get_user_todos():
+    res = requests.post(GOREST_USERS, data=userdata, headers=HEADER)
+    user_dict = res.json()
+    requests.post(GOREST_TODOS,
+                  data={"user_id": user_dict['id'],
+                        'title': 'TODO Title',
+                        'status': 'pending'},
+                  headers=HEADER)
+
+    get_user_todos = requests.get(GOREST_USERS + f"/{user_dict['id']}/todos", headers=HEADER)
+    assert get_user_todos.status_code == HTTPStatus.OK
+    requests.delete(GOREST_USERS + f"/{user_dict['id']}", headers=HEADER)
+
+
+def test_create_user():
     res = requests.post(GOREST_USERS, data=userdata, headers=HEADER)
     user_dict = res.json()
     assert res.status_code == HTTPStatus.CREATED
     requests.delete(GOREST_USERS + f"/{user_dict['id']}", headers=HEADER)
 
 
-def test_create_post(userdata, HEADER):
-    new_user = requests.post(GOREST_USERS, data=userdata, header=HEADER).json()
+def test_create_post():
+    new_user = requests.post(GOREST_USERS, data=userdata, headers=HEADER)
+    user_dict = new_user.json()
     new_post = requests.post(GOREST_POSTS,
-                             data={"user_id": new_user['id'],
+                             data={"user_id": user_dict['id'],
                                    'title': 'Min titel',
-                                   'body': 'detta är min body'},
+                                   'body': 'Detta är en post'},
                              headers=HEADER)
 
     assert new_post.status_code == HTTPStatus.CREATED
-    requests.delete(GOREST_POSTS + f"/{new_post.json()['id']}", headers=HEADER)
-    requests.delete(GOREST_USERS + f"/{new_user['id']}", headers=HEADER)
+    requests.delete(GOREST_USERS + f"/{user_dict['id']}", headers=HEADER)
 
 
-# def test_create_user():
-#     res = requests.post(GOREST_USERS, data={"name": "new tester",
-#                                             "email": "newtester@email.com",
-#                                             "gender": "male",
-#                                             "status": "active"},
-#                         headers=HEADER)
-#     user_json = res.json()
-#     assert res.status_code == HTTPStatus.CREATED
-#     assert 'name' in user_json
-#     assert 'email' in user_json
-#     assert 'gender' in user_json
-#     assert 'status' in user_json
-#     assert 'id' in user_json
-#     requests.delete(GOREST_USERS + f"/{user_json['id']}", headers=HEADER)
+def test_create_comment():
+    new_user = requests.post(GOREST_USERS, data=userdata, headers=HEADER)
+    user_dict = new_user.json()
+    new_post = requests.post(GOREST_POSTS,
+                             data={"user_id": user_dict['id'],
+                                   'title': 'Min titel',
+                                   'body': 'Detta är en post'},
+                             headers=HEADER)
+    post_dict = new_post.json()
+    new_comment = requests.post(GOREST_COMMENTS,
+                                data={"post_id": post_dict["id"],
+                                      "name": 'Commenter Name',
+                                      "email": 'commenter@email.com',
+                                      "body": 'This is a comment'},
+                                headers=HEADER)
 
+    assert new_comment.status_code == HTTPStatus.CREATED
+    requests.delete(GOREST_USERS + f"/{user_dict['id']}", headers=HEADER)
+
+
+def test_create_todo():
+    new_user = requests.post(GOREST_USERS, data=userdata, headers=HEADER)
+    user_dict = new_user.json()
+    new_todo = requests.post(GOREST_TODOS,
+                             data={"user_id": user_dict['id'],
+                                   'title': 'TODO Title',
+                                   'status': 'pending'},
+                             headers=HEADER)
+    todo_dict = new_todo.json()
+
+    assert new_todo.status_code == HTTPStatus.CREATED
+    requests.delete(GOREST_USERS + f"/{todo_dict['user_id']}", headers=HEADER)
+
+
+def test_patch_user():
+    new_user = requests.post(GOREST_USERS, data=userdata, headers=HEADER)
+    user_dict = new_user.json()
+    patch = requests.patch(GOREST_USERS + f"/{user_dict['id']}",
+                           data={'name': 'Patched Name',
+                                 'email': 'Patched@name.com',
+                                 'status': 'inactive'},
+                           headers=HEADER)
+
+    assert patch.status_code == HTTPStatus.OK
+    requests.delete(GOREST_USERS + f"/{user_dict['id']}", headers=HEADER)
+
+
+def test_put_user():
+    new_user = requests.post(GOREST_USERS, data=userdata, headers=HEADER)
+    user_dict = new_user.json()
+    put = requests.put(GOREST_USERS + f"/{user_dict['id']}",
+                       data={'name': 'Putted Name',
+                             'email': 'Putted@name.com',
+                             'status': 'inactive'},
+                       headers=HEADER)
+
+    assert put.status_code == HTTPStatus.OK
+    requests.delete(GOREST_USERS + f"/{user_dict['id']}", headers=HEADER)
